cfc2bfae9b35ae15e574fe55e103b3ee742a0bf9
Arthur <arthurarp@hotmail.com>
2020-11-30 01:22:33 -0300

    Adicionando o método de adicionar aresta

--- graph.js
+++ graph.js
@@ -14,5 +14,29 @@ class Graph {
 
     this.vertexs[id] = vertex;
   }
+
+  // OBS: parametro 'type' opcional (se nao for passado ele considera uma aresta orientada (a->c))
+  // se for passado o valor 'undirected' em 'type', ele alem de fazer a aresta (a->c) ele faz a aresta (c->a)
+  addEdge(origin, destiny, type, weight) {
+
+    let new_edge = {
+      origin: origin,
+      destiny: destiny,
+      weight: weight,
+    };
+    this.vertexs[origin]['neighbors'][destiny] = new_edge;
+    this.vertexs[origin]['degree']++;
+
+    if (type === 'undirected') {
+      let new_inverse_edge = {
+        origin: destiny,
+        destiny: origin,
+        weight: weight,
+      };
+
+      this.vertexs[destiny]['neighbors'][origin] = new_inverse_edge;
+      this.vertexs[destiny]['degree']++;
+    }
+  }
 }
 
