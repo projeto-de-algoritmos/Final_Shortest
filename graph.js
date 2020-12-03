3dadef815d9df209d1f50cc6dce3141401a31854
Arthur <arthurarp@hotmail.com>
2020-12-03 17:13:43 -0300

    Adicionando variavel pra pegar o numero de vertices e esqueleto da funcao shortestPath

--- graph.js
+++ graph.js
@@ -1,7 +1,8 @@
 class Graph {
-
   constructor() {
-    this.vertexs = {};  // implementado assim para poder achar qualquer nó em O(1) (passando o id)
+    this.vertexs = {}; // implementado assim para poder achar qualquer nó em O(1) (passando o id)
+    this.memoization = {};
+    this.totalNodes = 0;
   }
 
   // adição de um vertice 'id' no grafo
@@ -13,30 +14,35 @@ class Graph {
     };
 
     this.vertexs[id] = vertex;
+    this.totalNodes++;
   }
 
   // OBS: parametro 'type' opcional (se nao for passado ele considera uma aresta orientada (a->c))
   // se for passado o valor 'undirected' em 'type', ele alem de fazer a aresta (a->c) ele faz a aresta (c->a)
   addEdge(origin, destiny, type, weight) {
-
     let new_edge = {
       origin: origin,
       destiny: destiny,
       weight: weight,
     };
-    this.vertexs[origin]['neighbors'][destiny] = new_edge;
-    this.vertexs[origin]['degree']++;
+    this.vertexs[origin]["neighbors"][destiny] = new_edge;
+    this.vertexs[origin]["degree"]++;
 
-    if (type === 'undirected') {
+    if (type === "undirected") {
       let new_inverse_edge = {
         origin: destiny,
         destiny: origin,
         weight: weight,
       };
 
-      this.vertexs[destiny]['neighbors'][origin] = new_inverse_edge;
-      this.vertexs[destiny]['degree']++;
+      this.vertexs[destiny]["neighbors"][origin] = new_inverse_edge;
+      this.vertexs[destiny]["degree"]++;
     }
   }
-}
 
+  shortestPath(destiny) {
+    // iniciar com infinito e zerar somente a coluna destiny
+    // loops para verificar os menores caminhos
+    // find solution
+  }
+}
